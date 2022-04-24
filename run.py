7375983f8f4aadd196b18eb709006fdd92bda78d
Nyambura🌻 <bianca@Biancas-MacBook-Air.local>
2022-04-24 22:19:47 +0300

    add a run.py file

---
+++ run.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+from contact import Contact
+
+def create_contact(fname,lname,phone,email):
+    '''
+    Function to create a new contact
+    '''
+    new_contact = Contact(fname,lname,phone,email)
+    return new_contact
